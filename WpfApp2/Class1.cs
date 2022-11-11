af81ef37e87da0ef939cd0f01c03aa0cad93cc80
Amin <amin.boudlale.stud@ispascalcomandini.it>
2022-11-11 19:18:28 +0100

    Versione 1: creazione classe

---
+++ WpfApp2/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp2
+{
+    internal class Class1
+    {
+        public string nome;
+        public string cognome;
+        public Class1(string nome, string cognome)
+        {
+            this.nome = nome;
+            this.cognome = cognome;
+        }
+        public string getNome() { return nome; }
+        public string getCognome() { return cognome; }
+        public void setNome(string nome) { this.nome = nome; }
+        public void setCognome(string cognome) { this.cognome = cognome;}
+        public string toString() { return ("Il mio nome e' " +this.nome+ " il mio cognome e' " +this.cognome); }
+    }
+}
